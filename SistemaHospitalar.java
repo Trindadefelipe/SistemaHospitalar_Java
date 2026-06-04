37873b6cb9d1dd7114229f59d7282f20ad1e12ae
Felipe Trindade <felipe_biel3@hotmail.com>
2026-06-04 20:58:03 -0300

    feat: iniciando implementação do sistema

---
+++ SistemaHospitalar.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+abstract class Usuario{
+  protected String login;
+  protected String senha;
+  protected ArrayList <String> historicoSenhas;
+
+  public Usuario(String login, String senha){
+    this.login = login;
+    this.senha = senha;
+    this.historicoSenhas = new ArrayList<>();
+    this.historicoSenhas.add(senha);
+  }
+
+  public boolean verificarSenha(String tentativa){
+    return this.senha.equals(tentativa);
+  }
+
+  public boolean podeUsarSenha(String NovaSenha){
+    int inicio = Math.max(0, historicoSenhas.size() -3);
+    for(int i = inicio; i < historicoSenhas.size(); i++){
+      if (historicoSenhas.get(i).equals(NovaSenha)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void trocarSenha(String novaSenha){
+    historicoSenhas.add(novaSenha);
+    this.senha = novaSenha;
+  }
+
+  public String getLogin(){
+    return login;
+  }
+
+  public String getSenha(){
+    return senha;
+  }
+
+  public ArrayList <String> getHistoricoSenhas(){
+    return historicoSenhas;
+  }
+
+  public void setLogin(String login){
+    this.login = login;
+  }
+
+  public void setHistoricoSenhas(ArrayList<String> historico){
+    this.historicoSenhas = historico;
+  }
+
+  @Override
+  public String toString(){
+    return "Usuario{login='" + "'}";
+  }
+}
+
+abstract class Atendimento{
+  protected String paciente;
+  protected String medico;
+  protected String data;
+  protected double valor;
+
+  public Atendimento(String paciente, String medico, String data, double valor){
+    this.paciente = paciente;
+    this.medico = medico;
+    this.data = data;
+    this.valor = valor;
+  }
+
+  public void 
+
+}
+
+public class SistemaHospitalar {
+  public static void main(String[] args) {
+    
+  }
+}

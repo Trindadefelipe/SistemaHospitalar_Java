30ae415fb7752ef23bc37c17fe95385d7911588e
Felipe Trindade <felipe_biel3@hotmail.com>
2026-06-14 23:24:06 -0300

    feat: tarefa 3 finalizada

--- model/Atendimento.java
+++ model/Atendimento.java
@@ -1,16 +1,39 @@
 package model;
 
 public abstract class Atendimento{
-  protected String paciente;
-  protected String medico;
-  protected String data;
+  protected Paciente paciente;
+  protected Medico medico;
+  protected String dataAtendimento;
   protected double valor;
 
-  public Atendimento(String paciente, String medico, String data, double valor){
+  public Atendimento(Paciente paciente, Medico medico, String dataAtendimento, double valor){
     this.paciente = paciente;
     this.medico = medico;
-    this.data = data;
+    this.dataAtendimento = dataAtendimento;
     this.valor = valor;
   }
+
+  public Paciente getPaciente(){
+    return paciente;
+  }
+  
+  public Medico getMedico(){
+    return medico;
+  }
+  
+  public String getDataAtendimento(){
+    return dataAtendimento;
+  }
+  
+  public double getValor(){
+    return valor;
+  }
+
+  @Override
+  public String toString(){
+    return "Atendimento de " + paciente.getNomePaciente() + " com médico Dr(a) " + medico.getNomeMedico() + " em " + getDataAtendimento() + " - Valor: R$" + getValor();
+  }
+
+  public abstract double calcularCusto();
 
 }

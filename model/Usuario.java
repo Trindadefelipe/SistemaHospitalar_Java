9d506946d2136ce0623399a088066905431b9559
Higor Bueno <hbo.higor@gmail.com>
2026-06-15 19:30:55 -0300

    feat: Finalização plano Higor.

--- model/Usuario.java
+++ model/Usuario.java
@@ -2,25 +2,25 @@ package model;
 
 import java.util.ArrayList;
 
-public abstract class Usuario{
+public abstract class Usuario {
   protected String login;
   protected String senha;
-  protected ArrayList <String> historicoSenhas;
+  protected ArrayList<String> historicoSenhas;
 
-  public Usuario(String login, String senha){
+  public Usuario(String login, String senha) {
     this.login = login;
     this.senha = senha;
     this.historicoSenhas = new ArrayList<>();
     this.historicoSenhas.add(senha);
   }
 
-  public boolean verificarSenha(String tentativa){
+  public boolean verificarSenha(String tentativa) {
     return this.senha.equals(tentativa);
   }
 
-  public boolean podeUsarSenha(String NovaSenha){
-    int inicio = Math.max(0, historicoSenhas.size() -3);
-    for(int i = inicio; i < historicoSenhas.size(); i++){
+  public boolean podeUsarSenha(String NovaSenha) {
+    int inicio = Math.max(0, historicoSenhas.size() - 3);
+    for (int i = inicio; i < historicoSenhas.size(); i++) {
       if (historicoSenhas.get(i).equals(NovaSenha)) {
         return false;
       }
@@ -28,33 +28,36 @@ public abstract class Usuario{
     return true;
   }
 
-  public void trocarSenha(String novaSenha){
+  public void trocarSenha(String novaSenha) {
     historicoSenhas.add(novaSenha);
     this.senha = novaSenha;
   }
 
-  public String getLogin(){
+  public String getLogin() {
     return login;
   }
 
-  public String getSenha(){
+  public String getSenha() {
     return senha;
   }
 
-  public ArrayList <String> getHistoricoSenhas(){
+  public ArrayList<String> getHistoricoSenhas() {
     return historicoSenhas;
   }
 
-  public void setLogin(String login){
+  public void setLogin(String login) {
     this.login = login;
   }
 
-  public void setHistoricoSenhas(ArrayList<String> historico){
+  public void setHistoricoSenhas(ArrayList<String> historico) {
     this.historicoSenhas = historico;
   }
 
   @Override
-  public String toString(){
+  public String toString() {
     return "Usuario - login: " + getLogin() + " | senha: " + getSenha();
   }
+
+  public abstract String gerarLinhasCsv();
+
 }
---
+++ persistencia/GerenciadorArquivos.java
@@ -0,0 +1,64 @@
+package persistencia;
+
+import java.util.ArrayList;
+import model.Usuario;
+import model.Medico;
+import model.Paciente;
+import model.Plano;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class GerenciadorArquivos {
+
+    public void salvarUsuarios(ArrayList<Usuario> usuarios) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("arquivos/usuarios.txt"));
+            for (Usuario u : usuarios) {
+                bw.write(u.gerarLinhasCsv());
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar: " + e.getMessage());
+        }
+    }
+
+    public ArrayList<Usuario> carregarUsuarios() {
+        ArrayList<Usuario> lista = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("arquivos/usuarios.txt"));
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                String[] campos = linha.split(";");
+                String tipo = campos[0];
+                String login = campos[1];
+                String senha = campos[2];
+                String[] historico = campos[3].split(",");
+
+                Usuario u = null;
+                if (tipo.equals("M")) {
+                    u = new Medico(login, senha, campos[4], campos[5], campos[6]);
+                } else if (tipo.equals("P")) {
+                    Plano plano = new Plano(campos[6], Boolean.parseBoolean(campos[7]), Integer.parseInt(campos[8]));
+                    u = new Paciente(login, senha, campos[4], campos[5], plano);
+                }
+
+                if (u != null) {
+                    ArrayList<String> hist = new ArrayList<>();
+                    for (String s : historico)
+                        hist.add(s);
+                    u.setHistoricoSenhas(hist);
+                    lista.add(u);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Arquivo não encontrado, iniciando vazio.");
+        }
+        return lista;
+    }
+}
